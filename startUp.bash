d006b3358488af263f7de88851975c6b0c45fb00
alirezaUbuntuVm <alirezainallo@gmail.com>
2025-05-17 23:57:39 +0330

    initial commit, add bash continents

---
+++ startUp.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+for i in {1..30}; do
+    if vmhgfs-fuse -h > /dev/null 2>&1; then
+        break
+    fi
+    echo "Waiting for vmhgfs-fuse to be ready..."
+    sleep 2
+done
+
+sudo mount -t fuse.vmhgfs-fuse .host:/ /mnt/hgfs -o allow_other
+touch onStartUpCreated.txt
+
+echo "Mount command executed at $(date)" >> /tmp/mount.log
+echo "StartUp Task Worked"
